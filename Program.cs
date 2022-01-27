0935e82d63344e96d827aa765703fc245c1afa70
Dmitry Anoschenkov <danoschenkov@gmail.com>
2022-01-27 12:15:33 +0300

    Test resolved

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых
+// меньше или равна 3. 
+
+string[] myArray = new string[] {"monkey", "a", "12", "bee", "lion", "cat", "dog", "111", "!2&*^"};
+
+void PrintArray(string[] ExternalArray)
+{
+    for(int i = 0; i < ExternalArray.Length; i++)
+        Console.Write(ExternalArray[i] + " ");
+        Console.WriteLine();
+}
+
+string[] PickItemsFromArray(string[] ExternalArray)
+{
+    string[] ArrayResult = new string[ExternalArray.Length];
+
+    int counter = 0;
+    for (int i = 0; i < ExternalArray.Length; i++)
+    {
+        if (ExternalArray[i].Length <= 3) 
+        {
+            ArrayResult[counter] = ExternalArray[i];
+            counter = counter + 1;
+        }
+    }
+
+    Array.Resize(ref ArrayResult, counter);
+    return ArrayResult;
+}
+
+Console.WriteLine("Original array: ");
+PrintArray(myArray);
+
+string[] ArrayFinal = PickItemsFromArray(myArray);
+
+Console.WriteLine("Array with items with less than 3 symbols is:");
+PrintArray(ArrayFinal);
